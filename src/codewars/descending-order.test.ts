87140bee8b8a1cc5bc97267552f5ff0dc8e5da1d
rafi tanujaya <rafibussines@gmail.com>
2024-03-07 07:58:41 +0700

    add opsi non use sort build fn

--- src/codewars/descending-order.test.ts
+++ src/codewars/descending-order.test.ts
@@ -7,4 +7,26 @@ describe('descendingOrder', () => {
     expect(descendingOrder(123456789)).toBe(987654321)
 
   });
+
+  it('should barbar', () => {
+    function descendingOrder (n : number ) : number {
+
+        let arr = n.toString().split('').map(val => Number(val))
+
+        for(let i = 0; i < arr.length; i++) {
+            for(let j = i+1; j < arr.length; j++) {
+                if(arr[i] < arr[j]) {
+                    let temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp
+                }
+            }
+        }
+
+        return Number(arr.join(''))
+    }
+
+    expect(descendingOrder(123)).toBe(321)
+    expect(descendingOrder(123456789)).toBe(987654321)
+  });
 })

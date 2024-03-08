6638d9103f918430bafd236882886aa9aedd6f20
rafi tanujaya <rafibussines@gmail.com>
2024-03-08 07:31:14 +0700

    add opsi non temp

--- src/codewars/get-middle.test.ts
+++ src/codewars/get-middle.test.ts
@@ -1,7 +1,7 @@
 describe("Get Middle char in word", () => {
   it("should get middle char if word odd return 1 middle char if even return 2 middle char", () => {
-    function getMiddle(s: string) : string {
-      let middle:string = "";
+    function getMiddle(s: string): string {
+      let middle: string = "";
       if (s.length % 2 == 0) {
         middle += s[s.length / 2 - 1];
         middle += s[s.length / 2];
@@ -12,7 +12,18 @@ describe("Get Middle char in word", () => {
       }
     }
 
-    expect(getMiddle('testing')).toBe('t');
-    expect(getMiddle('test')).toBe('es');
+    expect(getMiddle("testing")).toBe("t");
+    expect(getMiddle("test")).toBe("es");
+  });
+
+  it("should non temp", () => {
+    function getMiddle(s: string): string {
+      return s.length % 2 == 0
+        ? s.substring(s.length / 2 - 1, s.length / 2 + 1)
+        : s.substring(Math.floor(s.length / 2), Math.ceil(s.length / 2));
+    }
+
+    expect(getMiddle("testing")).toBe("t");
+    expect(getMiddle("test")).toBe("es");
   });
 });

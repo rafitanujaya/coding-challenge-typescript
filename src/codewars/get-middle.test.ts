7af08232e1f7f71ce20af5e3dedbc6b33773ee02
rafi tanujaya <rafibussines@gmail.com>
2024-03-08 07:03:47 +0700

    get middle character

---
+++ src/codewars/get-middle.test.ts
@@ -0,0 +1,18 @@
+describe("Get Middle char in word", () => {
+  it("should get middle char if word odd return 1 middle char if even return 2 middle char", () => {
+    function getMiddle(s: string) : string {
+      let middle:string = "";
+      if (s.length % 2 == 0) {
+        middle += s[s.length / 2 - 1];
+        middle += s[s.length / 2];
+        return middle;
+      } else {
+        middle += s[Math.floor(s.length / 2)];
+        return middle;
+      }
+    }
+
+    expect(getMiddle('testing')).toBe('t');
+    expect(getMiddle('test')).toBe('es');
+  });
+});

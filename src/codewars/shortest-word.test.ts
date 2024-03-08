af3e9b6b6e9b3719d8a2d48ef5e15aefedb66134
rafi tanujaya <rafibussines@gmail.com>
2024-03-09 06:54:52 +0700

    shortest-word

---
+++ src/codewars/shortest-word.test.ts
@@ -0,0 +1,18 @@
+describe('Shortest-word', () => {
+  it('should option barbar', () => {
+    function findShort(s: string): number{
+        let temp: number[] = s.split(' ').map(val => val.length);
+        let sortWord : number =  temp[0]
+
+        for(let i = 0; i < temp.length; i++) {
+            if(sortWord > temp[i]) {
+                sortWord = temp[i]
+            }
+        }
+        return sortWord
+    }
+
+    expect(findShort('"turns out random test cases are easier than writing out basic ones"'))
+        .toBe(3)
+  });
+})

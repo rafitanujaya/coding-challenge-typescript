e3dbcd978982baf1dc65a4af9e6d4c1968f2d241
rafi tanujaya <rafibussines@gmail.com>
2024-03-12 06:57:07 +0700

    square digits

---
+++ src/codewars/squaredigits.test.ts
@@ -0,0 +1,22 @@
+describe('squareDigits', () => {
+  it('should barbar', () => {
+    function squareDigits(num: number) : number{
+        let numStr: string = String(num)
+        let temp: number[] = [];
+
+        for(let i = 0; i < numStr.length ; i++) {
+            let n: number = Number(numStr[i])
+            temp.push(n * n)
+        }
+        numStr = '';
+        for(let i = 0; i < temp.length; i++) {
+            numStr += String(temp[i]);
+        }
+
+        return Number(numStr)
+      }
+
+      expect(squareDigits(3212)).toBe(9414)
+    
+  });
+})

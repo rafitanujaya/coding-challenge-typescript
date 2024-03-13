84c0b00d7ba41a4007c6cb0de39c7bfe8a5e5cb5
rafi tanujaya <rafibussines@gmail.com>
2024-03-13 10:51:12 +0700

    Mumbling

---
+++ src/codewars/mumbling.test.ts
@@ -0,0 +1,23 @@
+describe("Mumbling", () => {
+  it("should barbar", () => {
+    function accum(s: string): string {
+      // your code
+      let temp: string[] = s.split("");
+      for (let i = 0; i < s.length; i++) {
+        const upper : string = temp[i];
+        temp[i] = upper.toUpperCase();
+      }
+      for (let i = 0; i < s.length; i++) {
+        const lower : string = temp[i];
+        for (let j = 0; j < i; j++) {
+          temp[i] += lower.toLowerCase();
+        }
+      }
+      return temp.join("-");
+    }
+
+    expect(accum("ZpglnRxqenU")).toBe(
+      "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+    );
+  });
+});

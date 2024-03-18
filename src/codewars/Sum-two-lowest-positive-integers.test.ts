6b4f9121d1e80a39eff8f3847f0ad99e028276b8
rafi tanujaya <rafibussines@gmail.com>
2024-03-18 10:58:40 +0700

    sum two lowest positive integers

---
+++ src/codewars/Sum-two-lowest-positive-integers.test.ts
@@ -0,0 +1,18 @@
+describe("Sum of two lowest positive integers", () => {
+  it("should barbar", () => {
+    function sumTwoSmallestNumbers(numbers: number[]): number {
+      for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+          if (numbers[i] > numbers[j]) {
+            let temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+          }
+        }
+      }
+      return numbers[0] + numbers[1];
+    }
+
+    expect(sumTwoSmallestNumbers([5, 8, 12, 19, 22])).toBe(13);
+  });
+});

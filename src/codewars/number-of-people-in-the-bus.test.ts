c61dbeb872cac451cb583b477a912ef391a7c44c
rafi tanujaya <rafibussines@gmail.com>
2024-04-02 14:33:46 +0700

    feat: adding one line

--- src/codewars/number-of-people-in-the-bus.test.ts
+++ src/codewars/number-of-people-in-the-bus.test.ts
@@ -12,4 +12,16 @@ describe('Number of People in the Bus', () => {
 
     expect(busStop([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])).toBe(17)
   });
+
+  it('should one line', () => {
+    function busStop(busStops: number[][]): number {
+        return busStops.reduce((people: number, halte: number[]) => {
+            const [inPeople, outPeople] = halte;
+            return people + inPeople - outPeople;
+        }, 0);
+    }
+    
+
+    expect(busStop([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])).toBe(17);
+  });
 })

a2df7b5625aae495c0d1f06e320f3df123edc5dc
rafi tanujaya <rafibussines@gmail.com>
2024-04-23 23:39:08 +0700

    feat: adding pass by refrence

--- src/codewars/coba.test.ts
+++ src/codewars/coba.test.ts
@@ -1,5 +1,24 @@
-describe('', () => {
-  it('should ', () => {
-    
+describe('coba aja dulu', () => {
+  it('should change user', () => {
+    interface User {
+      username:string;
+    }
+    let user : User  = {
+      username: 'rafi'
+    };
+    const editUser = (user : User) : void =>  {
+      user.username = `edit ${user.username}`
+    }
+    editUser(user)
+    expect(user.username).toBe('edit rafi')
+  });
+
+  it('should pass by refrence', () => {
+    const array1: number[] = [1,2,3]
+    const array2: number[] = array1
+
+    array2[0] = 2
+
+    expect(array1[0]).toBe(1)
   });
 })
